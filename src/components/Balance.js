c2de87f8f6ea3c2c263facf2d1ccb790975343e4
Hassan Ikram <58582095+Hassan-Ik@users.noreply.github.com>
2020-11-15 12:08:37 +0500

    Update Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,21 +1,20 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react'
 
-// import the Global State
+// Import the Global State
 import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
-    
-    const {transactions } = useContext(GlobalContext);
+
+    const { transactions } = useContext(GlobalContext);
 
     const transactionAmounts = transactions.map(transaction => transaction.transactionAmount);
-    const balance = transactionAmounts.reduce((acc, item) => (acc += item),0).toFixed(2);
+    const balance = transactionAmounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    
 
     return (
-        <div className="balance">
-            <h3>
-               Current Balance 
-            </h3>
-            <h3 id="balance">Rs 120000</h3>
+        <div>
+            <h4>Current Balance</h4>
+            <h1 id="balance">${balance}</h1>
         </div>
-    );
+    )
 }
